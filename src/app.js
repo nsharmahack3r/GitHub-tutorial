3cd1f3b51585911f55f410d57e3247b48d7aaf5d
Nabhya Sharma <nabhyasharma@Nabhyas-MacBook-Pro.local>
2024-01-31 23:02:36 +0530

    Greet feature built

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ app.get('/',(req, res)=>{
     res.send('Hello World');
 });
 
+app.get('/greet',(req, res)=>{
+    res.send('Greetings from DartBucket');
+});
+
 app.listen(3000,()=>{
     console.log("Server running on http://localhost:3000");
 });
